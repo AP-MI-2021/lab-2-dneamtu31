c1388d30f07341ed5d2b1f053404aa27a6d39097
daniel neamtu <dneamtu31@gmail.com>
2021-10-04 15:16:06 +0300

    commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_n_choose_k(n, k):
+    return 1
+def test_get_n_choose_k(n,k):
+    assert
+
+def main():
+    # interfata de tip consola aici
+    pass
+    print('Am implementat interfata!')
+
+
+if __name__ == '__main__':
+    main()
